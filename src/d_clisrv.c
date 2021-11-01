747ac5c36e2a82e5db46f1046339d7be55bcedb8
John FrostFox <john.frostfox@gmail.com>
2021-11-01 21:24:08 +0300

    Hashtable for savestates

--- src/d_clisrv.c
+++ src/d_clisrv.c
@@ -5784,7 +5784,6 @@ void MakeNetDebugString()
 		}
 	}
 
-	static tic_t lastSim = 0;
 	sprintf(&netDebugText[strlen(netDebugText)], "\n\nCanSimulate: %d", canSimulate);
 	sprintf(&netDebugText[strlen(netDebugText)], "\nJitter: %d", serverJitter);
 	sprintf(&netDebugText[strlen(netDebugText)], "\nRTTJitter: %d", rttJitter);
